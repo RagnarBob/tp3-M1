d6baba1032e81113641fba93031bb21ac78c1727
RagnarBob <makendyalexis5414@gmail.com>
2023-11-02 07:41:36 -0400

    mwen rive nan numewo 6

--- ASCII.py
+++ ASCII.py
@@ -9,3 +9,20 @@ print(var.split())
 #nimewo 3
 let = "the king ragnar"
 print(let.title())
+
+#nimewo 4
+chenn_nan = "mwen se yon Viking"
+varyab = chenn_nan.split()
+new_chenn = ""
+for el in varyab:
+    new_chenn += el[0]
+print(new_chenn)    
+
+#nimewo 5
+karak = "madame"
+print(karak.replace("a","@"))
+
+#nimewo 6
+vvar = "ThamArt"
+vvar_inv = vvar[::-1]
+print(vvar_inv)
